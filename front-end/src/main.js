52cd274ac0790b2f4665fd7c68db32d9470b4fff
doloshabell <22248628@mail.btpnsyariah.com>
2023-06-15 08:20:16 +0700

    wip: apply styling to main.js

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./main.css";
 import * as VueRouter from "vue-router";
 import ProductDetailPage from "./pages/ProductDetailPage.vue";
 import ProductsPage from "./pages/ProductsPage.vue";
